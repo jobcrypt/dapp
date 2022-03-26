99100fb712eac534ee76ec3beb32e3d31088c632
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2022-03-26 23:42:51 +0000

    Updated

--- solidity/openblock/IOpenPayments.sol
+++
@@ -1,9 +0,0 @@
-//"SPDX-License-Identifier: APACHE 2.0"
-
-pragma solidity >=0.8.0 <0.9.0;
-
-interface IOpenPayments {
-
-    function payForProduct(address _productAddress, uint256 _fee, address _erc20Address) payable external returns (bool _paid);
-
-}
