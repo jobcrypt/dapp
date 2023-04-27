896b5bb474f9817573845397c20eeca132bc11ec
Chiamaka Ugoji <mychiamaka@gmail.com>
2023-04-27 17:40:36 +0100

    Update events

--- builds/optimism_testnet/v0_2/assets/js/jc/automated_promo.js
+++ builds/optimism_testnet/v0_2/assets/js/jc/automated_promo.js
@@ -1,10 +1,10 @@
 const pagePromoSpot = ge("page_promo_spot");
 
 var munichOnline20230209 = {};
-munichOnline20230209.link =
-  "../../../pages/promotions/2023/job-coronation-nft.html";
+munichOnline20230209.link = "/pages/promotions/2023/job-coronation-nft.html";
 munichOnline20230209.title =
   "JobCrypt Web3 & Blockchain Talks - Introduction to Blockchain Ecosystems & Mining - (Germany) - Online - 09/02/2023";
+munichOnline20230209.btntxt = "Get Tickets";
 munichOnline20230209.startDate = 1675874700000;
 munichOnline20230209.endDate = 1675961100000;
 
@@ -13,6 +13,7 @@ liverpoolOnline20230228.link =
   "../../../pages/promotions/2023/job-coronation-nft.html";
 liverpoolOnline20230228.title =
   "JobCrypt Blockchain & Web3 Talks - Introducing Blockchain Streaming and Web3 Media Ecosystems - (UK) - Online - 28/02/2023";
+liverpoolOnline20230228.btntxt = "Get Tickets";
 liverpoolOnline20230228.startDate = 1677111697000;
 liverpoolOnline20230228.endDate = 1677543525000;
 
@@ -23,6 +24,7 @@ liverpoolInPerson20230315.link =
   "../../../pages/promotions/2023/job-coronation-nft.html";
 liverpoolInPerson20230315.title =
   "JobCrypt Web3 & Blockchain Talks - Introducing Blockchain and Decentralized Storage - Liverpool (UK) - In Person - 15/03/2023";
+liverpoolInPerson20230315.btntxt = "Get Tickets";
 liverpoolInPerson20230315.startDate = 1677629565000;
 liverpoolInPerson20230315.endDate = 1678902341000;
 
@@ -35,6 +37,7 @@ munichInPerson20230328.country = "Germany";
 munichInPerson20230328.date = "28th March 2023";
 munichInPerson20230328.startTime = "17:45";
 munichInPerson20230328.endTime = "21:00";
+munichInPerson20230328.btntxt = "Get Tickets";
 munichInPerson20230328.timezone = "CET";
 munichInPerson20230328.ticketsLink = "https://www.eventbrite.com/e/";
 munichInPerson20230328.startDate = 1677629565000;
@@ -54,6 +57,7 @@ munichOnline20230406.startTime = "17:45";
 munichOnline20230406.endTime = "20:00";
 munichOnline20230406.timezone = "CET";
 munichOnline20230406.ticketsLink = "http://bit.ly/40EiKYV";
+munichOnline20230406.btntxt = "Get Tickets";
 munichOnline20230406.startDate = 1677629565000;
 munichOnline20230406.endDate = 1681325100000;
 
@@ -74,7 +78,7 @@ liverpoolInPerson20230419.endDate = 1681922700000;
 // MAY
 var liverpoolCoronationCelebration20230508 = {};
 liverpoolCoronationCelebration20230508.scheduleLink =
-  "../../../pages/promotions/2023/job-coronation-nft.html";
+  "/pages/promotions/liverpool/2023-05-08-Coronation-Celebration.html";
 liverpoolCoronationCelebration20230508.title = "UK Coronation NFT Airdrop ";
 liverpoolCoronationCelebration20230508.location = "Online";
 liverpoolCoronationCelebration20230508.country = "UK";
@@ -84,12 +88,13 @@ liverpoolCoronationCelebration20230508.endTime = "20:00";
 liverpoolCoronationCelebration20230508.timezone = "BST";
 liverpoolCoronationCelebration20230508.ticketsLink =
   "https://discord.gg/Mmwq9QkPhY";
+liverpoolCoronationCelebration20230508.btntxt = "Join Discord";
 liverpoolCoronationCelebration20230508.startDate = 1678902341000;
 liverpoolCoronationCelebration20230508.endDate = 1683564300000;
 
 var liverpoolOnline20230511 = {};
 liverpoolOnline20230511.scheduleLink =
-  "/pages/promotions/liverpool/2023-05-11-Online-Talk.html";
+  "/pages/promotions/liverpool/2023-05-11-In-Person-Talk.html";
 liverpoolOnline20230511.title = "Introduction to DAOs in Sustainability";
 liverpoolOnline20230511.location = "Online";
 liverpoolOnline20230511.country = "UK";
@@ -98,11 +103,12 @@ liverpoolOnline20230511.startTime = "17:45";
 liverpoolOnline20230511.endTime = "20:00";
 liverpoolOnline20230511.timezone = "BST";
 liverpoolOnline20230511.ticketsLink = "https://bit.ly/40oaA6j";
+liverpoolOnline20230511.btntxt = "Get Tickets";
 liverpoolOnline20230511.startDate = 1680029100000;
 liverpoolOnline20230511.endDate = 1683823500000;
 
 var munichInPerson20230524 = {};
-munichInPerson20230524.scheduleLin =
+munichInPerson20230524.scheduleLink =
   "/pages/promotions/munich/2023-05-24-In-Person-Talk.html";
 munichInPerson20230524.title =
   "Introduction to Blockchain NFTs in Work, Entertainment and Social ";
@@ -113,6 +119,7 @@ munichInPerson20230524.startTime = "17:45";
 munichInPerson20230524.endTime = "21:00";
 munichInPerson20230524.timezone = "CET";
 munichInPerson20230524.ticketsLink = "https://bit.ly/3LcRziy";
+munichInPerson20230524.btntxt = "Get Tickets";
 munichInPerson20230524.startDate = 1680806700000;
 munichInPerson20230524.endDate = 1684946700000;
 
@@ -127,6 +134,7 @@ munichOnline20230608.country = "Germany";
 munichOnline20230608.date = "8th July 2023";
 munichOnline20230608.startTime = "17:45";
 munichOnline20230608.endTime = "20:00";
+munichOnline20230608.btntxt = "Get Tickets";
 munichOnline20230608.timezone = "CET";
 munichOnline20230608.ticketsLink = "https://www.eventbrite.com/e/";
 munichOnline20230608.startDate = 1681922700000;
@@ -143,6 +151,7 @@ liverpoolInPerson20230621.date = "21th June 2023";
 liverpoolInPerson20230621.startTime = "17:45";
 liverpoolInPerson20230621.endTime = "21:00";
 liverpoolInPerson20230621.timezone = "BST";
+liverpoolInPerson20230621.btntxt = "Get Tickets";
 liverpoolInPerson20230621.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolInPerson20230621.startDate = 1683564300000;
 liverpoolInPerson20230621.endDate = 1687365900000;
@@ -160,6 +169,7 @@ liverpoolOnline20230708.date = "8th July 2023";
 liverpoolOnline20230708.startTime = "17:45";
 liverpoolOnline20230708.endTime = "20:00";
 liverpoolOnline20230708.timezone = "BST";
+liverpoolOnline20230708.btntxt = "Get Tickets";
 liverpoolOnline20230708.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolOnline20230708.startDate = 1683823500000;
 liverpoolOnline20230708.endDate = 1688834700000;
@@ -175,6 +185,7 @@ munichInPerson20230719.date = "19th July 2023";
 munichInPerson20230719.startTime = "17:45";
 munichInPerson20230719.endTime = "21:00";
 munichInPerson20230719.timezone = "CET";
+munichInPerson20230719.btntxt = "Get Tickets";
 munichInPerson20230719.ticketsLink = "https://www.eventbrite.com/e/";
 munichInPerson20230719.startDate = 1684946700000;
 munichInPerson20230719.endDate = 1689785100000;
@@ -192,6 +203,7 @@ liverpoolInPerson20230806.date = "5th August 2023";
 liverpoolInPerson20230806.startTime = "12:45";
 liverpoolInPerson20230806.endTime = "21:00";
 liverpoolInPerson20230806.timezone = "BST";
+liverpoolInPerson20230806.btntxt = "Get Tickets";
 liverpoolInPerson20230806.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolInPerson20230806.startDate = 1683823500000;
 liverpoolInPerson20230806.endDate = 1688834700000;
@@ -207,6 +219,8 @@ munichOnline20230810.date = "10th August 2023";
 munichOnline20230810.startTime = "17:45";
 munichOnline20230810.endTime = "20:00";
 munichOnline20230810.timezone = "BST";
+munichOnline20230810.btntxt = "Get Tickets";
+
 munichOnline20230810.ticketsLink = "https://www.eventbrite.com/e/";
 munichOnline20230810.startDate = 1683823500000;
 munichOnline20230810.endDate = 1688834700000;
@@ -221,6 +235,7 @@ liverpoolInPerson20230823.country = "UK";
 liverpoolInPerson20230823.date = "23rd August 2023";
 liverpoolInPerson20230823.startTime = "17:45";
 liverpoolInPerson20230823.endTime = "21:00";
+liverpoolInPerson20230823.btntxt = "Get Tickets";
 liverpoolInPerson20230823.timezone = "CET";
 liverpoolInPerson20230823.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolInPerson20230823.startDate = 1684946700000;
@@ -236,6 +251,7 @@ munichInPerson20230826.country = "Germany";
 munichInPerson20230826.date = "26th August 2023";
 munichInPerson20230826.startTime = "12:45";
 munichInPerson20230826.endTime = "21:00";
+munichInPerson20230826.btntxt = "Get Tickets";
 munichInPerson20230826.timezone = "CET";
 munichInPerson20230826.ticketsLink = "https://www.eventbrite.com/e/";
 munichInPerson20230826.startDate = 1684946700000;
@@ -254,6 +270,7 @@ liverpoolOnline20230907.date = "8th July 2023";
 liverpoolOnline20230907.startTime = "17:45";
 liverpoolOnline20230907.endTime = "20:00";
 liverpoolOnline20230907.timezone = "BST";
+liverpoolOnline20230907.btntxt = "Get Tickets";
 liverpoolOnline20230907.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolOnline20230907.startDate = 1683823500000;
 liverpoolOnline20230907.endDate = 1688834700000;
@@ -268,6 +285,7 @@ munichInPerson20230920.country = "Germany";
 munichInPerson20230920.date = "20th September 2023";
 munichInPerson20230920.startTime = "17:45";
 munichInPerson20230920.endTime = "21:00";
+munichInPerson20230920.btntxt = "Get Tickets";
 munichInPerson20230920.timezone = "CET";
 munichInPerson20230920.ticketsLink = "https://www.eventbrite.com/e/";
 munichInPerson20230920.startDate = 1684946700000;
@@ -285,6 +303,7 @@ munichOnline20231005.country = "UK";
 munichOnline20231005.date = "8th July 2023";
 munichOnline20231005.startTime = "17:45";
 munichOnline20231005.endTime = "20:00";
+munichOnline20231005.btntxt = "Get Tickets";
 munichOnline20231005.timezone = "BST";
 munichOnline20231005.ticketsLink = "https://www.eventbrite.com/e/";
 munichOnline20231005.startDate = 1683823500000;
@@ -301,6 +320,7 @@ munichInPerson20231007.date = "19th July 2023";
 munichInPerson20231007.startTime = "17:45";
 munichInPerson20231007.endTime = "21:00";
 munichInPerson20231007.timezone = "CET";
+munichInPerson20231007.btntxt = "Get Tickets";
 munichInPerson20231007.ticketsLink = "https://www.eventbrite.com/e/";
 munichInPerson20231007.startDate = 1684946700000;
 munichInPerson20231007.endDate = 1689785100000;
@@ -316,6 +336,7 @@ liverpoolInPerson20231025.date = "19th July 2023";
 liverpoolInPerson20231025.startTime = "17:45";
 liverpoolInPerson20231025.endTime = "21:00";
 liverpoolInPerson20231025.timezone = "CET";
+liverpoolInPerson20231025.btntxt = "Get Tickets";
 liverpoolInPerson20231025.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolInPerson20231025.startDate = 1684946700000;
 liverpoolInPerson20231025.endDate = 1689785100000;
@@ -333,6 +354,7 @@ liverpoolInPerson20231104.date = "19th July 2023";
 liverpoolInPerson20231104.startTime = "17:45";
 liverpoolInPerson20231104.endTime = "21:00";
 liverpoolInPerson20231104.timezone = "CET";
+liverpoolInPerson20231104.btntxt = "Get Tickets";
 liverpoolInPerson20231104.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolInPerson20231104.startDate = 1684946700000;
 liverpoolInPerson20231104.endDate = 1689785100000;
@@ -348,6 +370,7 @@ liverpoolOnline20231109.date = "8th July 2023";
 liverpoolOnline20231109.startTime = "17:45";
 liverpoolOnline20231109.endTime = "20:00";
 liverpoolOnline20231109.timezone = "BST";
+liverpoolOnline20231109.btntxt = "Get Tickets";
 liverpoolOnline20231109.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolOnline20231109.startDate = 1683823500000;
 liverpoolOnline20231109.endDate = 1688834700000;
@@ -362,6 +385,7 @@ munichInPerson20231122.country = "Germany";
 munichInPerson20231122.date = "19th July 2023";
 munichInPerson20231122.startTime = "17:45";
 munichInPerson20231122.endTime = "21:00";
+munichInPerson20231122.btntxt = "Get Tickets";
 munichInPerson20231122.timezone = "CET";
 munichInPerson20231122.ticketsLink = "https://www.eventbrite.com/e/";
 munichInPerson20231122.startDate = 1684946700000;
@@ -379,6 +403,7 @@ liverpoolInPerson20231207.date = "7th December 2023";
 liverpoolInPerson20231207.startTime = "17:45";
 liverpoolInPerson20231207.endTime = "21:00";
 liverpoolInPerson20231207.timezone = "BST";
+liverpoolInPerson20231207.btntxt = "Get Tickets";
 liverpoolInPerson20231207.ticketsLink = "https://www.eventbrite.com/e/";
 liverpoolInPerson20231207.startDate = 1683823500000;
 liverpoolInPerson20231207.endDate = 1688834700000;
@@ -394,6 +419,7 @@ munichInPerson20231214.date = "14th December 2023";
 munichInPerson20231214.startTime = "17:45";
 munichInPerson20231214.endTime = "21:00";
 munichInPerson20231214.timezone = "CET";
+munichInPerson20231214.btntxt = "Get Tickets";
 munichInPerson20231214.ticketsLink = "https://www.eventbrite.com/e/";
 munichInPerson20231214.startDate = 1684946700000;
 munichInPerson20231214.endDate = 1689785100000;
@@ -517,7 +543,7 @@ function loadPromos() {
       schedule.append(link);
       link.setAttribute("href", event.scheduleLink);
       link.append(text("Schedule"));
-
+      console.log(event.scheduleLink + "");
       var tkts = ce("div");
       box.append(tkts);
       tkts.setAttribute("class", "btn-wrap");
@@ -527,7 +553,7 @@ function loadPromos() {
 
       tktsLink.setAttribute("href", event.ticketsLink);
       tktsLink.setAttribute("class", "btn-buy");
-      tktsLink.append(text("Get Tickets"));
+      tktsLink.append(text(event.btntxt));
 
       qualifierFactor++;
     }
