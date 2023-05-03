bf1e440a869766ac038f15ce1a7976e04956731a
Chiamaka Ugoji <mychiamaka@gmail.com>
2023-05-03 20:21:54 +0100

    Update pages

--- builds/optimism_testnet/ui/assets/js/jc/latest_jobs.js
+++ builds/optimism_testnet/ui/assets/js/jc/latest_jobs.js
@@ -49,7 +49,7 @@ function buildLatestJobs(postingAddresses) {
 
 function processRow(postingAddress) {
   var jobDetailLinkDestination =
-    "pages/app/job_detail_template.html?postingAddress=" + postingAddress;
+    "./app/job_detail_template.html?postingAddress=" + postingAddress;
 
   console.log(jobDetailLinkDestination);
   latestJobsTitle.innerHTML = "Latest Jobs";
