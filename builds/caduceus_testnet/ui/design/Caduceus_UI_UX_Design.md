16cda8282808926f0864e80fae023d84db776868
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2023-05-14 12:55:00 +0100

    updated

---
+++ builds/caduceus_testnet/ui/design/Caduceus_UI_UX_Design.md
@@ -0,0 +1,3 @@
+Figma link for Caduceus UI/UX Design v1.0
+
+https://www.figma.com/file/UUQ39V5Nlj2MY22jDSr768/JobCrypt---Caduceus?type=design&node-id=185%3A1798&t=vBaKy8vDCALuLZvp-1
---
+++ builds/reference_live/solidity/interfaces/IJobCryptStakeManager.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+
+interface IJobCryptStakeManager { 
+
+
+    function getMinimumStakeAmount() view external returns (uint256 _amount); 
+
+    function getStakedAmount() view external returns(uint256 _stakedAmount);
+
+    function isStaked(address _staker) view external returns (bool _staked);
+     
+    function stake(uint256 _amount) payable external returns (bool _staked);
+
+    function unstake() external returns (uint256 _unstakedAmount);
+
+    function getStakeErc20Address() external returns (address _stakeToken);
+
+
+}
